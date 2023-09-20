365aba7b4b99a5db5743f697131b99019265531b
waltereidi <walter-eidi@hotmail.com>
2023-09-20 20:56:22 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<script>
+export default {
+
+
+
+}
+</script>
+
+<template>
+  <div class="content">
+    <div class="content--header">
+
+
+    </div>
+    <div class="content--banner">
+      <div class="content--banner__content"></div>
+
+
+
+    </div>
+    <div class="content--main">
+      <div class="content--main__menuLeft">
+        <div class="item">
+          <p>a</p>
+        </div>
+        <div class="item">
+          <p>sdsdasda</p>
+        </div>
+        <div class="item">
+          <p>ssdffasdafzxcssdffasdafzxc</p>
+        </div>
+      </div>
+      <div class="content--main__mainContent">
+
+
+        <div class="form-Input_Wrapper">
+          <label for="film"> The film in question?</label>
+          <input id="film" name="film" type="text" placeholder="e.g. King Kong" required />
+
+
+        </div>
+
+      </div>
+
+    </div>
+    <div class="content--footer">
+
+
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+@import "@/assets/main.scss";
+</style>
